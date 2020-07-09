41750aca6832e5d2c337095910eccc1a5973c123
Chester Lau <chesterl@openpay.com.au>
2020-07-09 16:52:03 +1000

    Added functionality to search transactions

--- src/views/TransactionSearch/TransactionSearch.js
+++ src/views/TransactionSearch/TransactionSearch.js
@@ -1,12 +1,46 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 const TransactionSearch = () => {
 
+  const [blockNumber, changeBlockNumber] = useState();
+  const [address, changeAddress] = useState("");
   const [showLoader, changeShowLoader] = useState(false);
+
+  const handleBlockNumberTextChange = (event) => {
+    changeBlockNumber(event.target.value);
+  }
+
+  const handleAddressTextChange = (event) => {
+    changeAddress(event.target.value);
+  }
 
   const handleSubmit = event => {
     event.preventDefault();
-    console.log("Search submitted");
+
+    changeShowLoader(true);
+
+    const xtraceId = uuidv4();
+
+    const headers = {
+      "X-trace-ID": xtraceId
+    };
+
+    let payload = {
+      "blockNumber": parseInt(blockNumber),
+      "address": address
+    }
+
+    axios.post(`http://localhost:5000/api/search/Transactions`, payload, { headers: headers })
+      .then(res => {
+        console.log(res.data.transactions);
+        changeShowLoader(false);
+      })
+      .catch(ex => {
+        console.log(ex);
+        changeShowLoader(false);
+      });
   }
 
   return (
@@ -23,11 +57,11 @@ const TransactionSearch = () => {
           <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="blockNumberInput">Block number</label>
-              <input type="text" className="form-control" id="blockNumberInput" placeholder="Enter block number" />
+              <input type="text" className="form-control" id="blockNumberInput" placeholder="Enter block number" onChange={handleBlockNumberTextChange} />
             </div>
             <div className="form-group">
               <label htmlFor="addressInput">Address</label>
-              <input type="text" className="form-control" id="addressInput" placeholder="Enter address" />
+              <input type="text" className="form-control" id="addressInput" placeholder="Enter address" onChange={handleAddressTextChange} />
             </div>
             <button type="submit" className="btn btn-primary">Search</button>
           </form>
