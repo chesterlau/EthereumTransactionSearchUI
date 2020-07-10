dcc2a8abfbae2e7185c49d81f2f4bb022596d603
Chester Lau <chesterl@openpay.com.au>
2020-07-10 13:38:36 +1000

    Added functionality to display exponential numbers in full decimals

--- src/views/TransactionResult/TransactionResult.js
+++ src/views/TransactionResult/TransactionResult.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
-import { Truncate } from '../../utilities/Utility';
+import { Truncate, NoExponential } from '../../utilities/Utility';
 
 const TransactionResult = ({ transactionResults }) => {
 
-  function renderTooltip(props) {
+  const renderTooltip = props => {
     return (
       <Tooltip>
         {props}
@@ -48,22 +48,31 @@ const TransactionResult = ({ transactionResults }) => {
                           <span>{Truncate(item.blockHash, 20)}</span>
                         </OverlayTrigger>
                       </td>
-                      <td>{item.blockNumber}</td>
-                      <td>{item.gas}</td>
+                      <td><a href={`https://etherscan.io/txs?block=${item.blockNumber}`} target="_blank">{item.blockNumber}</a></td>
+                      <td>{NoExponential(item.gas)}</td>
                       <td><OverlayTrigger
                         placement="bottom"
                         delay={{ show: 250, hide: 400 }}
                         overlay={(renderTooltip(item.hash))}>
-                        <span>{Truncate(item.hash, 20)}</span>
+                        <a href={`https://etherscan.io/tx/${item.hash}`} target="_blank">{Truncate(item.hash, 20)}</a>
                       </OverlayTrigger></td>
-                      <td>{item.from}</td>
-                      <td>{item.to}</td>
-                      <td>{item.value}</td>
+                      <td><OverlayTrigger
+                        placement="bottom"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={(renderTooltip(item.from))}>
+                        <a href={`https://etherscan.io/address/${item.from}`} target="_blank">{Truncate(item.from, 20)}</a>
+                      </OverlayTrigger></td>
+                      <td><OverlayTrigger
+                        placement="bottom"
+                        delay={{ show: 250, hide: 400 }}
+                        overlay={(renderTooltip(item.to))}>
+                        <a href={`https://etherscan.io/address/${item.to}`} target="_blank">{Truncate(item.to, 20)}</a>
+                      </OverlayTrigger></td>
+                      <td>{NoExponential(item.value)}</td>
                     </tr>
                   ]
                 })
               }
-
             </tbody>
           </table>
         </div>
